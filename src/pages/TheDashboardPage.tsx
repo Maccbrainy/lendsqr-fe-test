75f4d663fd0d83048ba4ea0110565f0ddb96dcc0
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-01-30 08:43:54 +0100

    Resolves dashboard route links and updates

--- src/pages/TheDashboardPage.tsx
+++ src/pages/TheDashboardPage.tsx
@@ -1,30 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 import NavBar from "../components/NavBar";
 // import { FaToolbox } from "react-icons/fa";
 import { ImHome } from "react-icons/im";
 import { HiUsers, HiUserGroup } from "react-icons/hi2";
 
-// interface NavBarType {
-//     linkTitle?: string,
-//     linkIcon: any,
-//     linkName: string
-// }
-
-// const NavBarSection = ({linkTitle, linkIcon, linkName}:NavBarType) => {
-//     return (
-//     <div className="nav-section">
-//         <span className="nav-title">{linkTitle}</span>
-//         <div className="nav-main">
-//             <div className="active-link"></div>
-//             <div className="nav-link">
-//                 {linkIcon}
-//                 <span>{linkName}</span>
-//             </div>
-//         </div>
-//     </div> 
-//     )
-// }
 export default function TheDashboardPage(){
+    const { pathname } = useLocation();
     return (
         <>
         <NavBar/>
@@ -33,34 +14,45 @@ export default function TheDashboardPage(){
                 <div className="dashboard-layout-sidebar">
                     <nav className="flex flex-col">
                         <div className="nav-section">
-                            <div className="nav-main">
-                                <div className="nav-link">
-                                <ImHome/>
-                                    <span>Dashboard</span>
+                            <NavLink to={'/app'}>
+                                <div className={`nav-main ${pathname == '/app' && 'active'}`}>
+                                { pathname == '/app' && <div className="active-link"></div>}
+                                    <div className="nav-link">
+                                        <ImHome/>
+                                        <span>Dashboard</span>
+                                    </div>
                                 </div>
-                            </div>
+                            </NavLink>
                         </div>
                         <div className="nav-section">
                             <span className="nav-title">Customers</span>
-                            <div className="nav-main">
-                                <div className="active-link"></div>
-                                <div className="nav-link">
-                                    <HiUsers />
-                                    <span>Users</span>
+                            <NavLink to={'/app/users'}>
+                                <div className={`nav-main ${pathname == '/app/users' && 'active'}`}>
+                                { pathname == '/app/users' && <div className="active-link"></div>}
+                                    <div className="nav-link">
+                                        <HiUsers />
+                                        <span>Users</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="nav-main">
-                                <div className="active-link"></div>
+                            </NavLink>
+                            <div className={`nav-main`}>
                                 <div className="nav-link">
                                     <HiUserGroup />
                                     <span>Guarantors</span>
                                 </div>
                             </div>
                         </div>
-                        {/* <NavBarSection linkTitle={'Customers'} linkIcon={<HiUsers />} linkName={'Users'}/> */}
                     </nav>
                 </div>
                 <div className="dashboard-layout-mainbar">
+                    {
+                        pathname == '/app' &&
+                        <div>
+                            <h1>Dashboard Page</h1>
+                            <p>Welcome there!</p>
+                            <p>I am so excited on this opportunity! Thank you so much. Looking forward to working with you. God bless!</p>
+                        </div>
+                    }
                   <Outlet />
                 </div>
             </div>
