26f678190c7b5041fe063631ac13e85388a98006
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-01-30 08:40:09 +0100

    UserDetail page HTML and Css mockup

--- src/components/user-details/UserDetailsGeneralDetails.tsx
+++ src/components/user-details/UserDetailsGeneralDetails.tsx
@@ -1,7 +1,73 @@
+interface DetailInfo {
+    headerTitle: string,
+    headerText: string
+}
+const DetailContactSlot = ({headerTitle, headerText}: DetailInfo) => {
+    return (
+        <div className="slot">
+            <span className="header-title">{headerTitle}</span>
+            <span className="text">{headerText}</span>
+        </div>
+    )
+}
 export default function UserDetailsGeneralDetails(){
     return (
         <div className="UserDetailsGeneralDetails">
-            <h1>Personal Info etc...</h1>
+            <div className="details-section">
+              <h1 className="header-title">Personal Information</h1>
+              <div className="detailed-contact personal-section">
+                <DetailContactSlot headerTitle="Full name" headerText={'Grace Effiong'} />
+                <DetailContactSlot headerTitle="Phone Number" headerText={'09012345678'} />
+                <DetailContactSlot headerTitle="Email Address" headerText={'grace@gmail.com'} />
+                <DetailContactSlot headerTitle="BVN" headerText={'09012345678'} />
+                <DetailContactSlot headerTitle="Gender" headerText="Grace Effiong" />
+                <DetailContactSlot headerTitle="Marital Status" headerText={'09012345678'} />
+                <DetailContactSlot headerTitle="Children" headerText={'None'} />
+                <DetailContactSlot headerTitle="Type of Residence" headerText={'Parent\'s Apartment'} />
+                </div>
+            </div>
+            <div className="horizontal-divider"></div>
+            <div className="details-section">
+              <h1 className="header-title">Education and Employment</h1>
+              <div className="detailed-contact education-section">
+                <DetailContactSlot headerTitle="Level of Eduaction" headerText={'B. Sc'} />
+                <DetailContactSlot headerTitle="Employment" headerText={'Employed'} />
+                <DetailContactSlot headerTitle="Sector of Employment" headerText={'FinTech'} />
+                <DetailContactSlot headerTitle="Duration of Employment" headerText={'2 year'} />
+                <DetailContactSlot headerTitle="Office Email" headerText="grace@lendsqr.com" />
+                <DetailContactSlot headerTitle="Monthly Incone Status" headerText={'N200,000 - N 400, 000'} />
+                <DetailContactSlot headerTitle="Loan Repayment" headerText={'40, 000'} />
+                </div>
+            </div>
+            <div className="horizontal-divider"></div>
+            <div className="details-section">
+              <h1 className="header-title">Socials</h1>
+              <div className="detailed-contact social-section">
+                <DetailContactSlot headerTitle="Twitter" headerText={'B. Sc'} />
+                <DetailContactSlot headerTitle="Facebook" headerText={'FinTech'} />
+                <DetailContactSlot headerTitle="Instagram" headerText={'2 year'} />
+              </div>
+            </div>
+            <div className="horizontal-divider"></div>
+            <div className="details-section">
+              <h1 className="header-title">Guarantor</h1>
+              <div className="detailed-contact guarantor-section">
+                <DetailContactSlot headerTitle="Full name" headerText={'B. Sc'} />
+                <DetailContactSlot headerTitle="Phone Number" headerText={'FinTech'} />
+                <DetailContactSlot headerTitle="Email Address" headerText={'2 year'} />
+                <DetailContactSlot headerTitle="RelationShip" headerText={'2 year'} />
+              </div>
+            </div>
+            <div className="horizontal-divider"></div>
+            <div className="details-section">
+              {/* <h1 className="header-title">Guarantor</h1> */}
+              <div className="detailed-contact guarantor-section">
+                <DetailContactSlot headerTitle="Full name" headerText={'B. Sc'} />
+                <DetailContactSlot headerTitle="Phone Number" headerText={'FinTech'} />
+                <DetailContactSlot headerTitle="Email Address" headerText={'2 year'} />
+                <DetailContactSlot headerTitle="RelationShip" headerText={'2 year'} />
+              </div>
+            </div>
         </div>
     )
 }
--- src/components/user-details/UserDetailsHero.tsx
+++ src/components/user-details/UserDetailsHero.tsx
@@ -1,8 +1,46 @@
-import { Outlet } from "react-router-dom";
+import { HiArrowLongLeft, HiOutlineUser } from "react-icons/hi2";
+import { NavLink, Outlet } from "react-router-dom";
 export default function UserDetailsHero(){
     return (
         <div className="UserDetailsHero">
-            <h1>User Details</h1>
+            <NavLink to={'/app/users'} className="breadcrumb">
+                <HiArrowLongLeft />
+                <div>Back to Users</div>
+            </NavLink>
+            <div className="detail-actions">
+                <h1>User Details</h1>
+                <div className="actions-button">
+                    <button className="blacklist-user" title="Blacklist user" type="button">Blacklist user</button>
+                    <button className="activate-user" title="Activate user" type="button">Activate user</button>
+                </div>
+            </div>
+            <div className="detail-hero">
+                <div className="hero-summary">
+                    <div className="profile-detail">
+                        <div className="profile-image"><HiOutlineUser fontSize={40}/></div>
+                        <div className="profile-name">
+                            <span className="profile">Grace Effiong</span>
+                            <span className="profile-id">7yudrtERFD</span>
+                        </div>
+                    </div>
+                    <div className="profile-rating">
+                        <span>Users tiers</span>
+                        <span className="rating">Ratings</span>
+                    </div>
+                    <div className="profile-value">
+                        <div className="amount">N 2, 000, 000, 000</div>
+                        <div className="bank-id">98769999/Providus Bank</div>
+                    </div>
+                </div>
+                <div className="detail-hero-navigation">
+                    <NavLink className="hero-link" to={''}>General details</NavLink>
+                    <NavLink className="hero-link" to={'documents'}>Documents</NavLink>
+                    <NavLink className="hero-link" to={'bank-details'}>Bank details</NavLink>
+                    <NavLink className="hero-link" to={'loan'}>Loan</NavLink>
+                    <NavLink className="hero-link" to={'savings'}>Savings</NavLink>
+                    <NavLink className="hero-link" to={'app-and-systems'}>App and Systems</NavLink>
+                </div>
+            </div>
             <Outlet />
         </div>
     )
