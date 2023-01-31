f96a9607fa1c9bcb1d1c43378bdda22455931782
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-01-31 14:20:46 +0100

    Initial commit

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,13 @@
+export { default  as UsersListIcon } from "../icons/UsersListIcon.jsx";
+export { default as HomeIcon} from "../icons/HomeIcon.jsx";
+export { default as GuarantorIcon} from "../icons/GuarantorIcon.jsx";
+export { default as NoOfUsersIcon} from "../icons/NoOfUsersIcon.jsx";
+export { default as UsersWithLoanIcon} from "./UsersWithLoanIcon.jsx";
+export { default as UsersWithSavingsIcon} from "../icons/UsersWithSavingsIcon.jsx";
+export { default as ActiveUsersIcon} from "../icons/ActiveUsersIcon.jsx";
+export { default as BriefCaseIcon} from "../icons/BriefCaseIcon.jsx";
+export { default as ViewUserDetailsIcon} from "../icons/ViewUserDetailsIcon.jsx";
+export { default as BlackListUserIcon} from "../icons/BlackListUserIcon.jsx";
+export { default as ActivateUserIcon} from "../icons/ActivateUserIcon.jsx";
+export { default as FilterViewIcon} from "../icons/FilterViewIcon.jsx";
+export { default as ViewMoreIcon} from "../icons/ViewMoreIcon.jsx";
