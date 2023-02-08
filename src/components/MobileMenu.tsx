9faa6e74c1b0d56a6f3f589ccfa02edc0455e45f
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-02-08 19:28:06 +0100

    Responsive reusable mobile menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,11 +1,15 @@
 import { MdClose } from "react-icons/md";
+import AppNavigationSidebar from "./AppNavigationSidebar";
 interface MobileMenuProp {
     closeMobileMenuHandler: () => void
 }
 export default function MobileMenu({closeMobileMenuHandler}: MobileMenuProp){
     return(
         <div className="menu-container">
-            <div onClick={closeMobileMenuHandler}><MdClose color="#fff" fontSize={25}/></div>
+            <div className="close-menu" onClick={closeMobileMenuHandler}><MdClose fontSize={25}/></div>
+            <div className="menu-content">
+                <AppNavigationSidebar/>
+            </div>
         </div>
     )
 }
