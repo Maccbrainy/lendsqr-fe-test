23d8bb22d0bb4f608de7a1c10ba1140e0aecf0d4
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-02-08 12:48:53 +0100

    Code refactor, clean up and file modular structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
-import { ReactNode } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { Outlet, useLocation } from 'react-router-dom'
 import './App.scss'
-interface Props {
-  children?:ReactNode
-  // any props that come into the component
-}
+import NavBar from './components/NavBar'
+import AppNavigationSidebar from './components/AppNavigationSidebar'
+import { TheLoginPage } from './pages'
+import { useEffect } from 'react'
+// interface Props {
+//   children?:ReactNode
+//   // any props that come into the component
+// }
 
-function App({children}:Props) {
-
+function App() {
+// Create a react query client
+  const queryClient = new QueryClient();
+  const { pathname } = useLocation()
   return (
-    <div className="App">
-      <main>{children}</main>
-      <footer></footer>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='App'>
+        {
+          pathname == "/" ? (<TheLoginPage/>) :
+          (
+            <>
+            <NavBar />
+            <main>
+            <section className='app-page-layout'>
+              <AppNavigationSidebar />
+              <div className='app-layout-main'>
+                <Outlet />
+              </div>
+            </section>
+            </main>
+            <footer></footer>
+            </>
+          )
+        }
+      </div>
+    </QueryClientProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  RouterProvider,
+  RouterProvider 
 } from "react-router-dom";
 import router from './router'
-import App from './App'
 import './index.css'
-import { QueryClient, QueryClientProvider } from "react-query";
-// Create a react query client
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App><RouterProvider router={router} /></App>
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
