dbb663f482ad612049dc43b26d67422cf224d64d
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-02-08 12:29:32 +0100

    File moved to a different folder

--- src/components/user-details/index.ts
+++ src/components/user-details/index.ts
@@ -1,3 +1,2 @@
-export { default as UserDetailsLanding} from './UserDetailsLanding'
 export { default as UserDetailsDocuments} from './UserDetailsDocuments'
 export { default as UserDetailsGeneralDetails} from './UserDetailsGeneralDetails'
